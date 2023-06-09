78e442066081936d045b285dc76655d4ac19b643
Filipe de Oliveira <filipedeoliveira97@gmail.com>
2023-06-09 15:09:13 -0300

    css global

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+
+import './global.css';
 
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
+      <Header />
+
+      <Post 
+        author="Filipe Oliveira"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor enim voluptas distinctio quas saepe nemo nulla a error eaque, sint, qui quisquam commodi laudantium eum, provident in quam sed. Aperiam."
+      />
+      <Post
+        author="João Silva"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor enim voluptas distinctio quas saepe nemo nulla a error eaque, sint, qui quisquam commodi laudantium eum, provident in quam sed. Aperiam."
+      />
     </div>
   )
 }
