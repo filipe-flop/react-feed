ce94ef4daf8345593c12ea2b8f4e7147dcabf6a9
Filipe de Oliveira <filipedeoliveira97@gmail.com>
2023-06-11 23:04:56 -0300

    adicionados componentes de posts ao feed

---
+++ src/components/Post.jsx
@@ -0,0 +1,46 @@
+import styles from './Post.module.css';
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/filipe-flop.png" />
+                    <div className={styles.authorInfo}> 
+                        <strong>Filipe Oliveira</strong>
+                        <span>Software Engineer</span>
+                    </div>
+                </div>
+
+                <time title="11 de Junho às 21:57" dateTime="2023-06-11 21:58:00">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                
+                <p>Quam ab saepe impedit error, magnam quas assumenda recusandae earum animi laboriosam ex nisi perferendis?</p>
+                
+                <p>Pariatur aut itaque molestiae possimus blanditiis ipsa.</p>
+
+                <p>👉<a href="">test.design/link</a></p>
+
+                <p><a href="">#hashtag</a></p>
+                </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea
+                    placeholder="Deixe um comentário"
+                />
+
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
+        </article>
+    )
+}
+
