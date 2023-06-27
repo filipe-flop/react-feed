68e1d96f25ff11c40d254c0c3be422b7b00748da
Filipe Alan da Silva de Oliveira <fsoliveira@teconsuape.inet>
2023-06-27 17:32:46 -0300

    comentarios com useState

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,8 +5,15 @@ import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 import { enUS } from 'date-fns/locale';
+import { useState } from 'react';
 
 export function Post({author, publishedAt, content}) {
+
+    const [comments, setComments] = useState([
+        1,
+        2,
+    ])
+
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
     })
@@ -15,6 +22,12 @@ export function Post({author, publishedAt, content}) {
         locale: enUS,
         addSuffix: true,
     })
+
+    function handleCreateNewComment() {
+        event.preventDefault()
+
+        setComments([...comments, comments.length + 1])
+    }
 
     return (
         <article className={styles.post}>
@@ -42,7 +55,7 @@ export function Post({author, publishedAt, content}) {
                 })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Give feedback</strong>
 
                 <textarea
@@ -55,7 +68,9 @@ export function Post({author, publishedAt, content}) {
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
+                {comments.map(comment => {
+                    return <Comment />
+                })}
             </div>
         </article>
     )
